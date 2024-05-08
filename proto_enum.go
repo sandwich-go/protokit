1d0222f9525e26daf4ae8e9f3e78345ed609dc5d
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-05-08 16:53:26 +0800

    feat: 枚举类型增加option

--- proto_enum.go
+++ proto_enum.go
@@ -1,14 +1,16 @@
 package protokit
 
 import (
-	"strings"
-
 	"github.com/jhump/protoreflect/desc"
+	protokit2 "github.com/sandwich-go/protokit/option/gen/golang/protokit"
+	"google.golang.org/protobuf/proto"
+	"strings"
 )
 
 type ProtoEnumField struct {
 	Name   string
 	Number int32
+	Field  *desc.EnumValueDescriptor
 }
 type ProtoEnum struct {
 	dotFullyQualifiedTypeName string
@@ -26,10 +28,11 @@ func NewProtoEnum(pf *ProtoFile, ed *desc.EnumDescriptor) *ProtoEnum {
 		Name:                    ed.GetName(),
 		goNameWithGolangPackage: GoStructNameWithGolangPackage(ed.GetFullyQualifiedName(), pf.Package, pf.GolangPackageName),
 	}
-	for _, field := range v.ed.AsEnumDescriptorProto().Value {
+	for _, field := range v.ed.GetValues() {
 		v.Fields = append(v.Fields, &ProtoEnumField{
 			Name:   field.GetName(),
 			Number: field.GetNumber(),
+			Field:  field,
 		})
 	}
 	return v
@@ -48,4 +51,20 @@ func (p *Parser) BuildProtoEnum(pf *ProtoFile, ed *desc.EnumDescriptor) *ProtoEn
 	pe := NewProtoEnum(pf, ed)
 	pe.dotFullyQualifiedTypeName = p.descriptor2DotFullyQualifiedTypeName[pe.ed]
 	return pe
+}
+
+func (pe *ProtoEnum) GetOrmOption() *protokit2.OrmEnumOptions {
+	opts, ok := proto.GetExtension(pe.AsEnumDescriptor().GetEnumOptions(), protokit2.E_OrmEnum).(*protokit2.OrmEnumOptions)
+	if ok {
+		return opts
+	}
+	return nil
+}
+
+func (pe *ProtoEnumField) GetEnumValueOptions() *protokit2.EnumValueOptions {
+	opts, ok := proto.GetExtension(pe.Field.GetEnumValueOptions(), protokit2.E_EnumValue).(*protokit2.EnumValueOptions)
+	if ok {
+		return opts
+	}
+	return nil
 }
